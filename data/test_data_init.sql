0d054f5a5f286c7110d2b5d69fc56c7c66d6194f
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-04-12 15:06:17 -0400

    This sql is used to initialize some test data.

---
+++ data/test_data_init.sql
@@ -0,0 +1,25 @@
+--INSERT INTO `User` (`uid`, `uname`, `uaddress`, `uphone`, `ucity`) VALUES ('101', 'User1', '1 1st st', '1234567890', 'Brooklyn');
+
+-- All the data in this file is used for testing and all of the are created by developer
+
+/*
+  `semail` VARCHAR(20) NOT NULL,
+    `skey` VARCHAR(16) NOT NULL,
+    `sphone` VARCHAR(12) NOT NULL,
+    `sfirstname` VARCHAR(20) NOT NULL,
+    `slastname` VARCHAR(20) NOT NULL,
+    `suniversity` VARCHAR(40)  NULL,
+    `smajor` VARCHAR(5)  NULL,
+    `sgpa` VARCHAR(5)  NULL,
+    `sresume` VARCHAR(40)  NULL,
+
+  */
+
+INSERT INTO `Student` ('semail', `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
+VALUES ('dx1368@nyu.edu', `12345678`, `9998887777`, `Da`, `Xu`, `New York University`, `ECE`, `4.0`, `xxxxx`);
+INSERT INTO `Student` ('semail', `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
+VALUES ('cz1522@nyu.edu', `12345678`, `9998886666`, `Cong`, `Zhang`, `New York University`, `CS`, `4.0`, `xxxxx`);
+INSERT INTO `Student` ('semail', `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
+VALUES ('qy1449@nyu.edu', `12345678`, `9998884444`, `Qian`, `Yu`, `New York University`, `CS`, `4.0`, `xxxxx`);
+INSERT INTO `Student` ('semail', `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
+VALUES ('sy1567@nyu.edu', `12345678`, `9998882222`, `Song`, `Yan`, `New York University`, `ECE`, `2.8`, `xxxxx`);
