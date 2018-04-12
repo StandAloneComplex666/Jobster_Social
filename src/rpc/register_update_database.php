27cfcc931b1854563379d4323a6af8cfd849f96c
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-04-12 16:36:14 -0400

    finish the part which communicate with frontend

--- src/rpc/register_update_database.php
+++ src/rpc/register_update_database.php
@@ -1,15 +1,31 @@
 <?php
-	//This script is used to update the register information to backend database.
+//This script is used to update the register information to backend database.
 
-	//Fisrt check the user type: student user or company user.
-	$user_type = $_POST['usertype']
+//Fisrt check the user type: student user or company user.
+$user_type = $_POST['usertype']
 	if ($user_type == 'student'){
-		$sql_update = "INSERT INTO `Student` ('semail', `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
-					   VALUES ('dx1368@nyu.edu', `12345678`, `9998887777`, `Da`, `Xu`, `New York University`, `ECE`, `4.0`, `xxxxx`)";
-	}
-	elseif ($user_type == 'company') {
-		$sql_update = "INSERT INTO `Company` (`cname`, `ckey`, `cemail`, `clocation`, `cphone`, `cindusty`, `cdescription`)
-					   VALUES (`ZhuYuanzhang`, `1`, `yuanzhang@ming.com`, `Nanjing`, `1368139811`, `CS`, `xxxxx`);"
+        $semail = $_POST['semail'];
+        $skey = $_POST['skey'];
+        $sfirstname = $_POST['sfirstname'];
+        $slastname = $_POST['slastname'];
+        $sgpa = $_POST['sgpa'];
+        $sphone = $_POST['sphone'];
+        $suniversity = $_POST['univ'];
+        $smajor = $_POST['smajor'];
+        $sresume = $_POST['sresume'];
+        $sql_update = "INSERT INTO `Student` (`semail`, `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
+					   VALUES (`$semail`, `$skey`, `$sphone`, `$sfirstname`, `$slastname`, `$suniversity`, `$smajor`, `$sgpa`, `$sresume`)";
+    }
+    elseif ($user_type == 'company') {
+        $cname = $_POST['cname'];
+        $ckey = $_POST['ckey'];
+        $clocation = $_POST['clocation'];
+        $cemail = $_POST['cemail'];
+        $cphone = $_POST['cphone'];
+        $cindusty = $_POST['cindusty'];
+        $cdescription = $_POST['cdescription'];
+        $sql_update = "INSERT INTO `Company` (`cname`, `ckey`, `cemail`, `clocation`, `cphone`, `cindusty`, `cdescription`)
+					   VALUES (`$cname`, `$ckey`, `$cemail`, `$clocation`, `$cphone`, `$cindusty`, `$cdescription`);";
 
 	}
 
@@ -24,6 +40,8 @@
     if ($conn->connect_error) {
         die(json_encode(array('message' => "Connection failed: " . $conn->connect_error)));
     }
+
+    //send query of update new user to backend database.
     $double_check  = mysqli_query($conn, $sql_update);
     $conn->close();
 
