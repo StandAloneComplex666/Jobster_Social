48825660530089968488f9fe27a58ba00e2cf06d
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-04-12 16:36:36 -0400

    format correctness

--- data/test_data_init.sql
+++ data/test_data_init.sql
@@ -1,4 +1,4 @@
---INSERT INTO `User` (`uid`, `uname`, `uaddress`, `uphone`, `ucity`) VALUES ('101', 'User1', '1 1st st', '1234567890', 'Brooklyn');
+--INSERT INTO `User` (`uid`, `uname`, `uaddress`, `uphone`, `ucity`) VALUES (`101`, `User1`, `1 1st st`, `1234567890`, `Brooklyn`);
 
 -- All the data in this file is used for testing and all of the are created by developer
 
@@ -16,14 +16,14 @@
 
   */
 
-INSERT INTO `Student` ('semail', `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
-VALUES ('dx1368@nyu.edu', `12345678`, `9998887777`, `Da`, `Xu`, `New York University`, `ECE`, `4.0`, `xxxxx`);
-INSERT INTO `Student` ('semail', `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
-VALUES ('cz1522@nyu.edu', `12345678`, `9998886666`, `Cong`, `Zhang`, `New York University`, `CS`, `4.0`, `xxxxx`);
-INSERT INTO `Student` ('semail', `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
-VALUES ('qy1449@nyu.edu', `12345678`, `9998884444`, `Qian`, `Yu`, `New York University`, `CS`, `4.0`, `xxxxx`);
-INSERT INTO `Student` ('semail', `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
-VALUES ('sy1567@nyu.edu', `12345678`, `9998882222`, `Song`, `Yan`, `New York University`, `ECE`, `2.8`, `xxxxx`);
+INSERT INTO `Student` (`semail`, `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
+VALUES (`dx1368@nyu.edu`, `12345678`, `9998887777`, `Da`, `Xu`, `New York University`, `ECE`, `4.0`, `xxxxx`);
+INSERT INTO `Student` (`semail`, `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
+VALUES (`cz1522@nyu.edu`, `12345678`, `9998886666`, `Cong`, `Zhang`, `New York University`, `CS`, `4.0`, `xxxxx`);
+INSERT INTO `Student` (`semail`, `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
+VALUES (`qy1449@nyu.edu`, `12345678`, `9998884444`, `Qian`, `Yu`, `New York University`, `CS`, `4.0`, `xxxxx`);
+INSERT INTO `Student` (`semail`, `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
+VALUES (`sy1567@nyu.edu`, `12345678`, `9998882222`, `Song`, `Yan`, `New York University`, `ECE`, `2.8`, `xxxxx`);
 
 /*
     format of company:
