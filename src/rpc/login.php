af98861cea3c9daee461c7240d4d948fcbe423d6
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-04-12 17:24:53 -0400

    login.php is used when a user want to login to our website and check if the username exists and keyword matches the username.

---
+++ src/rpc/login.php
@@ -0,0 +1,46 @@
+<?php
+	//login page,check if the username and keywords are valid.
+	$username = $_POST['username'];
+	$keywords = $_POST['keywords'];
+	$user_type = $_POST['usertype'];
+	if ($user_type == 'student'){
+		$sql_check_username_exist = "select semail from Student where seamil = '$username'";
+		$sql_keywords_match = "select semail, skey from Student where semail = '$username' and skey = '$keywords';";
+	}
+	elseif ($user_type == 'company') {
+		$sql_check_username_exist = "select cname from Company where cname = '$username'";
+		$sql_keywords_match = "select cname, ckey from Company where cname = '$username' and ckey = '$keywords';";
+	}
+
+    //the parameters that used for connecting to database.
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "jobster";
+
+    //create new connection and check if it is connected successfully.
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die(json_encode(array('message' => "Connection failed: " . $conn->connect_error)));
+    }
+
+    $result_username_exist = mysqli_query($conn, $sql_check_username_exist);
+    if ($result_username_exist->num_rows > 0)
+    {
+    	$result_keywords_match = mysqli_query($conn, $sql_keywords_match);
+    	if ($result_keywords_match->num_rows > 0)
+        {
+            $response = "Login successfully!";
+        }
+        else
+        {
+    	    $response = "The keyword is not correct!";
+        }
+    }
+    else{
+    	$response = "The username has not been registered.";
+    }
+
+
+
+?>
