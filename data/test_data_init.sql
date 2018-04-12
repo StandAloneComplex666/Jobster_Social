f2f2335423413be8de08a3b6eec5f1705c79842f
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-04-12 15:28:19 -0400

    add some intial data of company, add a sample of each table.

--- data/test_data_init.sql
+++ data/test_data_init.sql
@@ -3,7 +3,8 @@
 -- All the data in this file is used for testing and all of the are created by developer
 
 /*
-  `semail` VARCHAR(20) NOT NULL,
+    format of student:
+    `semail` VARCHAR(20) NOT NULL,
     `skey` VARCHAR(16) NOT NULL,
     `sphone` VARCHAR(12) NOT NULL,
     `sfirstname` VARCHAR(20) NOT NULL,
@@ -23,3 +24,22 @@ INSERT INTO `Student` ('semail', `skey`, `sphone`, `sfirstname`, `slastname`, `s
 VALUES ('qy1449@nyu.edu', `12345678`, `9998884444`, `Qian`, `Yu`, `New York University`, `CS`, `4.0`, `xxxxx`);
 INSERT INTO `Student` ('semail', `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`)
 VALUES ('sy1567@nyu.edu', `12345678`, `9998882222`, `Song`, `Yan`, `New York University`, `ECE`, `2.8`, `xxxxx`);
+
+/*
+    format of company:
+    `cname` VARCHAR(45) NOT NULL,
+    `ckey` VARCHAR(45) NOT NULL,
+    `cemail` VARCHAR(10) NOT NULL,
+    `clocation` VARCHAR(45) NOT NULL,
+    `cphone` VARCHAR(12) NOT NULL,
+    `cindusty` VARCHAR(45) NULL,
+    `cdescription` VARCHAR(200) NULL,
+*/
+INSERT INTO `Company` (`cname`, `ckey`, `cemail`, `clocation`, `cphone`, `cindusty`, `cdescription`)
+VALUES (`ZhuYuanzhang`, `1`, `yuanzhang@ming.com`, `Nanjing`, `1368139811`, `CS`, `xxxxx`);
+INSERT INTO `Company` (`cname`, `ckey`, `cemail`, `clocation`, `cphone`, `cindusty`, `cdescription`)
+VALUES (`ZhuDi`, `2`, `di@ming.com`, `Nanjing`, `1402142411`, `CS`, `xxxxx`);
+INSERT INTO `Company` (`cname`, `ckey`, `cemail`, `clocation`, `cphone`, `cindusty`, `cdescription`)
+VALUES (`ZhuHoucong`, `11`, `Houcong@ming.com`, `Beijing`, `1521156611`, `CS`, `xxxxx`);
+INSERT INTO `Company` (`cname`, `ckey`, `cemail`, `clocation`, `cphone`, `cindusty`, `cdescription`)
+VALUES (`ZhuZaiji`, `12`, `Zaiji@ming.com`, `Beijing`, `1566157211`, `CS`, `xxxxx`);
