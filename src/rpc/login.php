7590ea600f4fe48db031a467cdae40933d788880
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-04-13 18:23:30 -0400

    change the way it responses errors to frontend (add header)

--- src/rpc/login.php
+++ src/rpc/login.php
@@ -1,49 +1,43 @@
 <?php
-	//login page,check if the username and keywords are valid.
-	$username = $_POST['username'];
-	$keywords = $_POST['keywords'];
-	$user_type = $_POST['usertype'];
-	if ($user_type == 'student'){
-		$sql_check_username_exist = "select semail from Student where seamil = '$username'";
-		$sql_keywords_match = "select semail, skey from Student where semail = '$username' and skey = '$keywords';";
-	}
-	elseif ($user_type == 'company') {
-		$sql_check_username_exist = "select cname from Company where cname = '$username'";
-		$sql_keywords_match = "select cname, ckey from Company where cname = '$username' and ckey = '$keywords';";
-	}
+//login page,check if the username and keywords are valid.
+$username = $_POST['username'];
+$keywords = $_POST['keywords'];
+$user_type = $_POST['usertype'];
 
-    //the parameters that used for connecting to database.
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "jobster";
+if ($user_type == 'student') {
+    $sql_check_username_exist = "select semail from Student where semail = '$username'";
+    $sql_keywords_match = "select semail, skey from Student where semail = '$username' and skey = '$keywords';";
+} else if ($user_type == 'company') {
+    $sql_check_username_exist = "select cname from Company where cname = '$username'";
+    $sql_keywords_match = "select cname, ckey from Company where cname = '$username' and ckey = '$keywords';";
+}
 
-    //create new connection and check if it is connected successfully.
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die(json_encode(array('message' => "Connection failed: " . $conn->connect_error)));
+//the parameters that used for connecting to database.
+$servername = "localhost";
+$dbusername = "root";
+$password = "root";
+$dbname = "jobster";
+
+//create new connection and check if it is connected successfully.
+$conn = new mysqli($servername, $dbusername, $password, $dbname);
+if ($conn->connect_error) {
+    die(json_encode(array('message' => "Connection failed: " . $conn->connect_error)));
+}
+
+$result_username_exist = mysqli_query($conn, $sql_check_username_exist);
+if ($result_username_exist->num_rows > 0) {
+    $result_keywords_match = mysqli_query($conn, $sql_keywords_match);
+    if ($result_keywords_match->num_rows > 0) {
+        $response = "Login successfully!";
+        echo $response;
+    } else {
+        header('HTTP/1.0 403 Forbidden');
+        die('The keyword is not correct!');
     }
+} else {
+    header('HTTP/1.0 403 Forbidden');
+    die("The username has not been registered.");
+}
 
-    $result_username_exist = mysqli_query($conn, $sql_check_username_exist);
-    if ($result_username_exist->num_rows > 0)
-    {
-    	$result_keywords_match = mysqli_query($conn, $sql_keywords_match);
-    	if ($result_keywords_match->num_rows > 0)
-        {
-            $response = "Login successfully!";
-            echo json_encode($response);
-        }
-        else
-        {
-    	    $response = "The keyword is not correct!";
-            echo json_encode($response);
-        }
-    }
-    else{
-    	$response = "The username has not been registered.";
-        echo json_encode($response);
-    }
-
-
-
+echo $response;
 ?>
