d09605fa0c958e319c150eb58f82f12aa2c3be7a
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-04-13 20:14:48 -0400

    modify some names and some typical error

--- src/rpc/login.php
+++ src/rpc/login.php
@@ -29,16 +29,17 @@ $usertype = 'student';
 
 if ($user_type == 'student') {
     $sql_check_username_exist = "select semail from Student where semail = '$username'";
-    $sql_keywords_match = "select semail, skey from Student where semail = '$username' and skey = '$keywords';";
-} else if ($user_type == 'company') {
+    $sql_passwords_match = "select semail, skey from Student where semail = '$username' and skey = '$passwords';";
+}
+elseif ($user_type == 'company'){
     $sql_check_username_exist = "select cname from Company where cname = '$username'";
-    $sql_keywords_match = "select cname, ckey from Company where cname = '$username' and ckey = '$keywords';";
+    $sql_passwords_match = "select cname, ckey from Company where cname = '$username' and ckey = '$passwords';";
 }
 
 //the parameters that used for connecting to database.
 $servername = "localhost";
 $dbusername = "root";
-$password = "root";
+$password = "";
 $dbname = "jobster";
 
 //create new connection and check if it is connected successfully.
@@ -49,8 +50,8 @@ if ($conn->connect_error) {
 
 $result_username_exist = mysqli_query($conn, $sql_check_username_exist);
 if ($result_username_exist->num_rows > 0) {
-    $result_keywords_match = mysqli_query($conn, $sql_keywords_match);
-    if ($result_keywords_match->num_rows > 0) {
+    $result_passwords_match = mysqli_query($conn, $sql_passwords_match);
+    if ($result_passwords_match->num_rows > 0) {
         $response = "Login successfully!";
         echo $response;
     } else {
