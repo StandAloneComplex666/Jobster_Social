2fe79f7c6419683e165bbb896055d76f59018680
Stand Alone Complex <yc2739@nyu.edu>
2018-04-18 20:12:45 -0400

    add the connection to database

--- src/rpc/company/selectStudentPost.php
+++ src/rpc/company/selectStudentPost.php
@@ -5,3 +5,17 @@
  * Date: 2018/4/17
  * Time: 23:51
  */
+//the parameters that used for connecting to database.
+$servername = "localhost";
+$dbusername = "root";
+$password = "root";
+$dbname = "jobster";
+
+//create new connection and check if it is connected successfully.
+$conn = new mysqli($servername, $dbusername, $password, $dbname);
+if ($conn->connect_error) {
+    die(json_encode(array('message' => "Connection failed: " . $conn->connect_error)));
+}
+
+$conn->close();
+?>
