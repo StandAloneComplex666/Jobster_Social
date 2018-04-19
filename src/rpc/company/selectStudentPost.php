9550d2dd8dd327d550610630660e8b1968f9d45d
Stand Alone Complex <yc2739@nyu.edu>
2018-04-19 14:37:56 -0400

    need to be finished later for the array variable between frontend and backend need to be discussed

--- src/rpc/company/selectStudentPost.php
+++ src/rpc/company/selectStudentPost.php
@@ -5,6 +5,32 @@
  * Date: 2018/4/17
  * Time: 23:51
  */
+class personal_info{
+    public $semail;
+    public $skey;
+    public $sphone;
+    public $sfirstname;
+    public $slastname;
+    public $suniversity;
+    public $smajor;
+    public $sgpa;
+    public $sresume;
+}
+
+function Build_personal_Info($row)
+{
+    $personalInfo = new personal_info();
+    $personalInfo->seamil = $row['semail'];
+    $personalInfo->sphone = $row['sphone'];
+    $personalInfo->sfirstname = $row['sfirstname'];
+    $personalInfo->slastname = $row['slastname'];
+    $personalInfo->suniversity = $row['suniversity'];
+    $personalInfo->smajor = $row['smajor'];
+    $personalInfo->sgpa = $row['sgpa'];
+    $personalInfo->sresume = $row['sresume'];
+    return $personalInfo;
+}
+
 //the parameters that used for connecting to database.
 $servername = "localhost";
 $dbusername = "root";
@@ -16,6 +42,15 @@ $conn = new mysqli($servername, $dbusername, $password, $dbname);
 if ($conn->connect_error) {
     die(json_encode(array('message' => "Connection failed: " . $conn->connect_error)));
 }
+
+//getparameters from frontend.
+$cname = $_POST['cname'];
+$jid = $_POST['jid'];
+
+//query all the student that followed and update notification
+
+$sql_post_selected_student = "INSERT INTO notification(`nid`, `companysend`, `semailreceive`, `jid`, `pushtime`, `status`)
+VALUES ('', '$cname', '$semail[1]', '$jid', CURRENT_DATE, 'unviewed');";
 
 $conn->close();
 ?>
