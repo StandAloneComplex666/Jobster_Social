49e1faef0b643e930284e2cf9683df154330de31
Stand Alone Complex <yc2739@nyu.edu>
2018-04-18 20:44:50 -0400

    no message

--- src/rpc/company/student_info.php
+++ src/rpc/company/student_info.php
@@ -5,3 +5,18 @@
  * Date: 2018/4/18
  * Time: 20:42
  */
+//the parameters that used for connecting to database.
+$servername = "localhost";
+$dbusername = "root";
+$password = "";
+$dbname = "jobster";
+
+//create new connection and check if it is connected successfully.
+$conn = new mysqli($servername, $dbusername, $password, $dbname);
+if ($conn->connect_error) {
+    die(json_encode(array('message' => "Connection failed: " . $conn->connect_error)));
+}
+
+//get parameter from forntend
+//query student info from backend database if the company accepts the application.
+$sql_student_info = "select suniversity, smajor, sgpa, sresume from Student where ";
