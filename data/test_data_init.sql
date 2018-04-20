efbe31ea064aa1c9655e4cdf19b150580bb05c37
Stand Alone Complex <yc2739@nyu.edu>
2018-04-20 19:49:52 -0400

    no message

--- data/test_data_init.sql
+++ data/test_data_init.sql
@@ -24,6 +24,10 @@ INSERT INTO `Student` (`semail`, `skey`, `sphone`, `sfirstname`, `slastname`, `s
 VALUES ('qy1449@nyu.edu', '12345678', '9998884444', 'Qian', 'Yu', 'New York University', 'CS', '4.0', 'xxxxx',TRUE);
 INSERT INTO `Student` (`semail`, `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`,`sprivacy`)
 VALUES ('sy1567@nyu.edu', '12345678', '9998882222', 'Song', 'Yan', 'New York University', 'ECE', '2.8', 'xxxxx',FALSE);
+INSERT INTO `Student` (`semail`, `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`,`sprivacy`)
+VALUES ('xp@gmail.com', '12345678', '9998882222', 'XP', 'Windows', 'New York University', 'ECE', '3.5', 'xxxxx',FALSE);
+INSERT INTO `Student` (`semail`, `skey`, `sphone`, `sfirstname`, `slastname`, `suniversity`, `smajor`, `sgpa`, `sresume`,`sprivacy`)
+VALUES ('95@gmail.com', '12345678', '9998882222', '95', 'Windows', 'Washington University', 'CS', '3.2', 'xxxxx',FALSE);
 
 /*
     format of company:
@@ -38,14 +42,15 @@ VALUES ('sy1567@nyu.edu', '12345678', '9998882222', 'Song', 'Yan', 'New York Uni
 INSERT INTO `Company` (`cname`, `ckey`, `cemail`, `clocation`, `cphone`, `cindustry`, `cdescription`)
 VALUES ('ZhuYuanzhang', '1', 'yuanzhang@ming.com', 'Nanjing', '1368139811', 'CS', 'xxxxx');
 INSERT INTO `Company` (`cname`, `ckey`, `cemail`, `clocation`, `cphone`, `cindustry`, `cdescription`)
-VALUES ('ZhuDi', '2', 'di@ming.com', 'Nanjing', '1402142411', 'CS', 'xxxxx');
+VALUES ('ZhuDi', '1', 'di@ming.com', 'Nanjing', '1402142411', 'CS', 'xxxxx');
 INSERT INTO `Company` (`cname`, `ckey`, `cemail`, `clocation`, `cphone`, `cindustry`, `cdescription`)
-VALUES ('ZhuHoucong', '11', 'Houcong@ming.com', 'Beijing', '1521156611', 'CS', 'xxxxx');
+VALUES ('ZhuHoucong', '1', 'Houcong@ming.com', 'Beijing', '1521156611', 'CS', 'xxxxx');
 INSERT INTO `Company` (`cname`, `ckey`, `cemail`, `clocation`, `cphone`, `cindustry`, `cdescription`)
-VALUES ('ZhuZaiji', '12', 'Zaiji@ming.com', 'Beijing', '1566157211', 'CS', 'xxxxx');
+VALUES ('ZhuZaiji', '1', 'Zaiji@ming.com', 'Beijing', '1566157211', 'CS', 'xxxxx');
+INSERT INTO `Company` (`cname`, `ckey`, `cemail`, `clocation`, `cphone`, `cindustry`, `cdescription`)
+VALUES ('Microsoft', '1', 'HR@ms.com', 'Seattle', '9998887777', 'CS', 'xxxxx');
 
 /*format of job
-
     `jid` VARCHAR(10) NOT NULL,
     `jlocation` VARCHAR(45) NULL,
     `jtitle` VARCHAR(45) NULL,
@@ -56,9 +61,11 @@ VALUES ('ZhuZaiji', '12', 'Zaiji@ming.com', 'Beijing', '1566157211', 'CS', 'xxxx
     `jdescription` VARCHAR(200),
 */
 INSERT INTO `JobAnnouncement` (`jid`, `jlocation`, `jtitle`, `jsalary`, `jreq_diploma`, `jreq_experience`, `jreq_skills`, `jdescription`)
-VALUES ('1', 'Beijing', 'Software Engineer', '120K', 'Bachelor', '5', 'JAVA', 'XXXXXX');
+VALUES ('1', 'Beijing', 'Software Engineer', '120K', 'Bachelor MS', '5', 'JAVA', 'XXXXXX');
 INSERT INTO `JobAnnouncement` (`jid`, `jlocation`, `jtitle`, `jsalary`, `jreq_diploma`, `jreq_experience`, `jreq_skills`, `jdescription`)
-VALUES ('2', 'Nanjing', 'Software Engineer', '100K', 'Bachelor', '3', 'C++', 'XXXXXX');
+VALUES ('2', 'Nanjing', 'Software Engineer', '100K', 'Bachelor CS', '3', 'C++', 'XXXXXX');
+INSERT INTO `JobAnnouncement` (`jid`, `jlocation`, `jtitle`, `jsalary`, `jreq_diploma`, `jreq_experience`, `jreq_skills`, `jdescription`)
+VALUES ('3', 'Seattle', 'Software Engineer', '100K', 'Master CS', '3', 'C++', 'XXXXXX');
 
 /*format of companypostjob
     `cname` VARCHAR(45) NOT NULL,
@@ -67,6 +74,7 @@ VALUES ('2', 'Nanjing', 'Software Engineer', '100K', 'Bachelor', '3', 'C++', 'XX
 */
 INSERT INTO `CompanyPostJob` (`cname`, `jid`, `posttime`) VALUES ('ZhuHoucong', '1', '2018-04-19');
 INSERT INTO `CompanyPostJob` (`cname`, `jid`, `posttime`) VALUES ('ZhuYuanzhang', '2', '2018-04-19');
+INSERT INTO `CompanyPostJob` (`cname`, `jid`, `posttime`) VALUES ('Microsoft', '3', '2018-04-19');
 
 /*format of studentfollowcompany
     `semail` VARCHAR(20) NOT NULL,
@@ -74,6 +82,8 @@ INSERT INTO `CompanyPostJob` (`cname`, `jid`, `posttime`) VALUES ('ZhuYuanzhang'
 */
 INSERT INTO `StudentFollowCompany` (`semail`, `cname`) VALUES ('sy1567@nyu.edu', 'ZhuHoucong');
 INSERT INTO `StudentFollowCompany` (`semail`, `cname`) VALUES ('dx1368@nyu.edu', 'ZhuYuanzhang');
+INSERT INTO `StudentFollowCompany` (`semail`, `cname`) VALUES ('xp@gmail.com', 'Microsoft');
+INSERT INTO `StudentFollowCompany` (`semail`, `cname`) VALUES ('95@gmail.com', 'Microsoft');
 
 /*format of studentapplyjob
     `aid` VARCHAR(10) NOT NULL,
@@ -94,6 +104,7 @@ VALUES('1', 'dx1368@nyu.edu', '2' ,'ZhuYuanzhang', 'unviewed', '2018-04-11');
 */
 INSERT INTO `StudentFriends` VALUES ('dx1368@nyu.edu', 'cz1522@nyu.edu', 'unviewed', '2018-04-03');
 INSERT INTO `StudentFriends` VALUES ('qy1449@nyu.edu', 'cz1522@nyu.edu', 'unviewed', '2018-04-02');
+INSERT INTO `StudentFriends` VALUES ('cz1522@nyu.edu', 'sy1567@nyu.edu', 'unviewed', '2018-01-01');
 
 /*format of notification
     `nid` VARCHAR(10) NOT NULL,
@@ -121,3 +132,5 @@ INSERT INTO `Message` (`mid`, `semailsend`, `semailreceive`, `content`, `sendtim
 VALUES('1','dx1368@nyu.edu', 'cz1522@nyu.edu',"gaige ruhe", '2018-04-15', 'unviewed');
 INSERT INTO `Message` (`mid`, `semailsend`, `semailreceive`, `content`, `sendtime`, `status`)
 VALUES('1','cz1522@nyu.edu', 'qy1522@nyu.edu',"beijing baoweizhan chenggong lema", '2018-04-15', 'unviewed');
+
+
