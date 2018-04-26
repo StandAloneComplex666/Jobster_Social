85ceba89a7adb538c43389477be071fafc8f184d
Stand Alone Complex <yc2739@nyu.edu>
2018-04-26 15:31:57 -0400

    no message

--- src/backend/rpc/company/search.php
+++ src/backend/rpc/company/search.php
@@ -31,45 +31,34 @@ $keyword = $_POST['keyword'];
 $sgpalower = $_POST['sgpalower'];
 $sgpahigh = $_POST['sgpahigh'];
 
-function search_resume($conn,$keyword)
-{
-    $response = array();
-    $sql_resume_path = "select semail,sresume from Student;";
-    $result_resume_path = mysqli_query($conn, $sql_resume_path);
-    if ($result_resume_path->num_rows > 0) {
-        while ($row = $result_resume_path->fetch_assoc()) {
-            $PDF_reader->setFilename($row['sresume']);
-            $PDF_reader->decodePDF();
-            if (strpos($PDF_reader->output(), $keyword)) {
-                $response[$row['semail']] = $row['sresume'];
-            }
+$response = array();
+//query from backend database to find the students that fit the keywords;
+$sql_search_student = "select * from student where suniversity like '%$keyword%' or (sgpa between '$sgpalower' and '$sgpahigh')
+or smajor like '%$keyword%';";
+$result_search_student = mysqli_query($conn, $sql_search_student);
+if ($result_search_student->num_rows > 0){
+    while($row = $result_search_student->fetch_assoc()){
+        $info = $objectStudentInfo->Build_personal_Info($row);
+        $response[$row['semail']] = $info;
+    }
+}
+//query resume that fit the keyword
+$sql_resume_path = "select * from Student;";
+$result_resume_path = mysqli_query($conn, $sql_resume_path);
+if ($result_resume_path->num_rows > 0) {
+    while ($row = $result_resume_path->fetch_assoc()) {
+        $PDF_reader->setFilename($row['sresume']);
+        $PDF_reader->decodePDF();
+        if (strpos($PDF_reader->output(), $keyword)) {
+            $info = $objectStudentInfo->Build_personal_Info($row);
+            $response[$row['semail']] = $info;
         }
     }
-    else {
+}
+else {
 //    echo 'error!';
-    }
-    return $response;
 }
 
-function search_student_label($conn, $keyword, $sgpalower, $sgpahigh){
-    $response = array();
-    //query from backend database to find the students that fit the keywords;
-    $sql_search_student = "select * from student where suniversity like '%$keyword%' or (sgpa between '$sgpalower' and '$sgpahigh')
-or smajor like '%$keyword%';";
-    $result_search_student = mysqli_query($conn, $sql_search_student);
-    if ($result_search_student->num_rows > 0){
-        while($row = $result_search_student->fetch_assoc()){
-            $info = $objectStudentInfo->Build_personal_Info($row);
-            array_push($temp_array, $info);
-        }
-        $response['student_info'] = $temp_array;
-    }
-    else{
-        $response['student_info'] = []  ;
-    }
-    return $response;
-}
-if
 echo json_encode($response);
 
 $conn->close();
