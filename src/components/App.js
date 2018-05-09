6275b44fbe9a6405753e48705d5fe08984b995ba
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-05-08 21:16:01 -0400

    no message

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,23 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 import { Header } from './Header';
 import { Main } from './Main';
-import { TOKEN_KEY } from '../constants';
+import { TOKEN_KEY, USER_NAME } from '../constants';
 
 
 class App extends Component {
     state = {
-        isLoggedIn: true,//!!localStorage.getItem(TOKEN_KEY),//true,//
-        // username: '',
+        isLoggedIn: !!localStorage.getItem(TOKEN_KEY),//true,//
+        username: '',
         //username: 'dx1368@nyu.edu',
         //username: 'cz1522@nyu.edu',
         //username: 'qy1449@nyu.edu',
         //username: 'sy1567@nyu.edu',
-        username: 'ZhuYuanzhang',
+        //username: 'ZhuYuanzhang',
     }
 
     handleLogin = (response, username) => {
-        //localStorage.setItem(TOKEN_KEY, token);
+        localStorage.setItem(TOKEN_KEY, response);
+        localStorage.setItem(USER_NAME, username);
         console.log(response);
         this.setState({
             isLoggedIn: true,
@@ -34,7 +35,7 @@ class App extends Component {
         return (
             <div className="App">
                 <Header isLoggedIn={this.state.isLoggedIn} handleLogout={this.handleLogout}/>
-                <Main isLoggedIn={this.state.isLoggedIn} handleLogin={this.handleLogin} username={this.state.username}/>
+                <Main isLoggedIn={this.state.isLoggedIn} handleLogin={this.handleLogin} username={localStorage.getItem(USER_NAME)}/>
             </div>
         );
     }
